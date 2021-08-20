8d7c75a7a7f4b9863ea1b94611ca5e7875e22976
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-08-20 20:12:51 +0300

    add array to hold qoutes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'daily-quote';
+  title = 'Daily Qoute | Stay Motivated';
+
+  qoutes:Quote[] = [
+    {id:1, authorName:'Amerix',qouteDescription: 'xxxxx'},
+    {id:2,authorName:'Nelson Mandela',qouteDescription: 'xxxxx'},
+    {id:3,authorName:'Lucky Dube', qouteDescription: 'xxxxx'},
+    {id:4,authorName:'Fidel Munywoki',qouteDescription: 'xxxxx'},
+    {id:5,authorName:'Michael Jordan', qouteDescription: 'xxxxx'},
+    {id:6,authorName: 'Michael B Jordan', qouteDescription: 'xxxxx'},
+  ];
 }

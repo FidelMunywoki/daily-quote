ff79d16b7b613c38aa9047910be91a4aa56d2aea
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-08-21 00:30:13 +0300

    add toggle function

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,13 +9,15 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    {id:1, authorName:'Amerix',quoteDescription: '"A winner is a dreamer who never gives up." "Education is the most powerful weapon which you can use to change the world."'},
-    {id:2,authorName:'Nelson Mandela',quoteDescription: '"A winner is a dreamer who never gives up." "Education is the most powerful weapon which you can use to change the world."'},
-    {id:3,authorName:'Lucky Dube', quoteDescription: '"A winner is a dreamer who never gives up." "Education is the most powerful weapon which you can use to change the world."'},
-    {id:4,authorName:'Fidel Munywoki',quoteDescription: '"A winner is a dreamer who never gives up." "Education is the most powerful weapon which you can use to change the world."'},
-    {id:5,authorName:'Michael Jordan', quoteDescription: '"A winner is a dreamer who never gives up." "Education is the most powerful weapon which you can use to change the world."'},
-    {id:6,authorName: 'Michael B Jordan', quoteDescription: '"A winner is a dreamer who never gives up." "Education is the most powerful weapon which you can use to change the world."'},
+    new Quote (1, "A winner is a dreamer who never gives up. Education is the most powerful weapon which you can use to change the world.", "Nelson Mandela"),
+    new Quote (2, "A winner is a dreamer who never gives up. Education is the most powerful weapon which you can use to change the world.", "Nelson Mandela"),
+    new Quote (3, "A winner is a dreamer who never gives up. Education is the most powerful weapon which you can use to change the world.", "Nelson Mandela"),
+    
   ];
+
+  toggleDetails(index: number) {
+    this.quotes[index].showAuthorName = !this.quotes[index].showAuthorName;
+  }
 
   constructor() { }
 
